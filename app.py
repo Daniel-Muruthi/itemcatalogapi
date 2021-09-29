15bc0e81830624b41b95ad900c4c00c688dd3c1b
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-09-29 12:10:59 +0300

    This will add the logic

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 import os
+from flask_migrate import Migrate
+
 
 
 # This will initialize the app
@@ -11,49 +13,38 @@ import os
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] ='catalog123'
-
-# configure sql
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://moringa:muruthi1995@localhost/catalog'
 app.config["DEBUG"] = True
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# configure sql
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://moringa:muruthi1995@localhost/catalog'
 
 api = Api(app)
 db = SQLAlchemy(app)
+Migrate(app,db)
+
 
 ###################################################################
-items = [
-    {
-        'id': 1,
-        'name': 'colgate',
-        'description': 'Your favorite toothpaste',
-        'imageurl': "https://www.seekpng.com/ipng/u2q8u2o0u2q8y3y3_colgate-png-image-colgate-strong-teeth-toothpaste/",
-        'price':230 
-    }
-]
+# items = [
+#     {
+#         'id': 1,
+#         'name': 'colgate',
+#         'description': 'Your favorite toothpaste',
+#         'imageurl': "https://www.seekpng.com/ipng/u2q8u2o0u2q8y3y3_colgate-png-image-colgate-strong-teeth-toothpaste/",
+#         'price':230 
+#     }
+# ]
 ###################################################################
 
 # db model for users
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(255))
     public_id=db.Column(db.String(255), unique=True)
     email = db.Column(db.String(255))
     password = db.Column(db.String(255))
-
-    @property
-    def jsfrmt(self):
-        '''
-        Will return data in a json format
-        '''
-        return {
-            'id' : self.id,
-            'name': self.name,
-            'public_id': self.public_id,
-            'email': self.email,
-            'password': self.password,
-
-        }
 
 
 #db model for item categories
@@ -63,15 +54,6 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
-    @property
-    def jsfrmt(self):
-        '''
-        Will return data in a json format
-        '''
-        return {
-            'id' : self.id,
-            'name': self.name
-        }
 
 # db Model for items
 class Item(db.Model):
@@ -82,18 +64,6 @@ class Item(db.Model):
     imageurl = db.Column(db.String(255))
     price = db.Column(db.Integer)
 
-    @property
-    def jsfrmt(self):
-        '''
-        Will return data in a json format
-        '''
-        return {
-            'id' : self.id,
-            'name': self.name,
-            'description': self.description,
-            'imageurl' : self.imageurl,
-            'price' : self.price
-        }
 
 
 ###########Routes####################
@@ -104,19 +74,19 @@ def user():
 
     all_users =[]
 
-    for item in items:
+    for user in users:
         name_list = {}
-        name_list.id = item['id']
-        name_list.name = item['name']
-        name_list.description = item['description']
-        name_list.imageurl = item['imageurl']
-        name_list.price = item['price']
+        name_list['id'] = user.id
+        name_list['name'] = user.name
+        name_list['public_id'] = user.public_id
+        name_list['email'] = user.email
+        name_list['password'] = user.password
 
         all_users.append(name_list)
 
     return jsonify({'items': all_users})
 
-@app.route('/user', method=['POST'])
+@app.route('/user', methods=['POST'])
 def create_user():
     data = request.get_json()
 
@@ -136,11 +106,11 @@ def item():
 
     for item in items:
         item_list = {}
-        item_list.id = item['id']
-        item_list.name = item['name']
-        item_list.description = item['description']
-        item_list.imageurl = item['imageurl']
-        item_list.price = item['price']
+        item_list['id'] = item.id
+        item_list['name'] = item.name
+        item_list['description'] = item.description
+        item_list['imageurl'] = item.imageurl
+        item_list['price'] = item.price
 
         all_items.append(item_list)
 
@@ -154,15 +124,40 @@ def category():
 
     for category in categories:
         category_list = {}
-        category_list.id = category['id']
-        category_list.name = category['name']
+        category_list['id'] = category.id
+        category_list['name'] = category.name
 
         all_categories.append(category_list)
 
 
     return jsonify({'categories': all_categories})
 
+@app.route('/category/all', methods=['POST'])
+def all_categories():
+    categories = Category.query.all()
 
+    if not categories:
+        return jsonify({'message': 'There are no categories!'})
 
-if __name__=="__main__":
+    user.admin = True
+    db.session.add(categories)
+    db.session.commit()
+
+    return jsonify({'message': 'No categories have been added'})
+
+@app.route('/user/<public_id>', methods=['DELETE'])
+def deleteuser(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'User you are trying to delete does not exist'})
+
+    user.admin = True
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'User has been successfully deleted'})
+
+if __name__ == '__main__':
     app.run()
+
+
