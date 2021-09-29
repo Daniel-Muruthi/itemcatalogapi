82a7921b1058e2d09e3dc9dcd4a11922c4bd77f0
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-09-29 08:38:30 +0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+import os
+
+
+# This will initialize the app
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] ='catalog123'
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://moringa:muruthi1995@localhost/catalog'
+app.config["DEBUG"] = True
+
+api = Api(app)
+db = SQLAlchemy(app)
+
+###################################################################
+items = [
+    {
+        'id': 1,
+        'name': 'colgate',
+        'description': 'Your favorite toothpaste',
+        'imageurl': "https://www.seekpng.com/ipng/u2q8u2o0u2q8y3y3_colgate-png-image-colgate-strong-teeth-toothpaste/",
+        'price':230 
+    }
+]
+###################################################################
+
+# db model for users
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(255))
+    public_id=db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    admin=db.Column(db.Boolean)
+
+    @property
+    def jsfrmt(self):
+        '''
+        Will return data in a json format
+        '''
+        return {
+            'id' : self.id,
+            'name': self.name,
+            'public_id': 
+        }
+
+
+#db model for item categories
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+    @property
+    def jsfrmt(self):
+        '''
+        Will return data in a json format
+        '''
+        return {
+            'id' : self.id,
+            'name': self.name
+        }
+
+# db Model for items
+class Item(db.Model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(255))
+    imageurl = db.Column(db.String(255))
+    price = db.Column(db.Integer)
+
+    @property
+    def jsfrmt(self):
+        '''
+        Will return data in a json format
+        '''
+        return {
+            'id' : self.id,
+            'name': self.name,
+            'description': self.description,
+            'imageurl' : self.imageurl,
+            'price' : self.price
+        }
+
+@app.route('/', methods=['GET'])
+def index():
+    pass
